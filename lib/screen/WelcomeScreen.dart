1d6218ba878799e9b34f892dd1e778265114f86d
Johannes Franke <jofra@hotmail.de>
2023-02-14 14:39:19 +0100

    initial Welcome Page

---
+++ lib/screen/WelcomeScreen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_application_3/screen/HomeScreen.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreen();
+}
+
+class _WelcomeScreen extends State<WelcomeScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          const Divider(height: 50, color: Colors.transparent),
+          const Text(
+              "Willkommen bei Klausur-Reminder",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold
+            ),
+          ),
+          const Spacer(),
+          Center(
+            child: TextButton(
+              child: const Text("Los geht's!"),
+              onPressed: () => Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (_) => const HomeScreen())
+              ),
+            ),
+          ),
+          const Divider(height: 50, color: Colors.transparent),
+        ],
+      )
+    );
+  }
+}
