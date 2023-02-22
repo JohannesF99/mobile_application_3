6d6cf95f223e331446d4a6decf4b1a098122396b
Johannes Franke <jofra@hotmail.de>
2023-02-22 14:19:02 +0100

    added local notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_application_3/screen/HomeScreen.dart';
 import 'package:mobile_application_3/screen/WelcomeScreen.dart';
@@ -5,6 +6,20 @@ import 'package:mobile_application_3/util/SharedPrefs.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  AwesomeNotifications().initialize(
+      null,
+      [
+        NotificationChannel(
+            channelGroupKey: 'basic_channel_group',
+            channelKey: 'basic_channel',
+            channelName: 'Basic notifications',
+            channelDescription: 'Notification channel for basic tests',
+            defaultColor: Colors.black,
+            ledColor: Colors.white
+        )
+      ],
+      debug: true
+  );
   final isFirst = await SharedPrefs.getBool("firstRun") ?? true;
   runApp(MyApp(isFirst: isFirst));
   SharedPrefs.saveBool("firstRun", false);
