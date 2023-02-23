0cc860a1c653d51eef6a990c760f22bd76e54d50
Johannes Franke <jofra@hotmail.de>
2023-02-23 17:51:33 +0100

    created static function to get a list of all notifications for one specific reminder

--- lib/util/Notifier.dart
+++ lib/util/Notifier.dart
@@ -1,16 +1,18 @@
 import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:mobile_application_3/model/NotificationDetail.dart';
 
 import '../model/Reminder.dart';
 
 class Notifier{
-  static create(Reminder rem) {
+  static void create(Reminder rem) {
     AwesomeNotifications().setChannel(NotificationChannel(
         channelKey: rem.title,
         channelName: rem.title,
         channelDescription: rem.title
     ));
-    [1, 7, 14]
-        .map((e) => rem.date.subtract(Duration(days: e)))
+    //TODO
+    [5, 15, 25]
+        .map((e) => rem.date.subtract(Duration(seconds: e)))
         .forEach((e) async => AwesomeNotifications().createNotification(
         content: NotificationContent(
             id: e.hashCode,
@@ -23,8 +25,22 @@ class Notifier{
     ));
   }
 
-  static delete(Reminder rem) {
+  static void delete(Reminder rem) {
     AwesomeNotifications().cancelNotificationsByChannelKey(rem.title);
     AwesomeNotifications().removeChannel(rem.title);
+  }
+
+  static Future<List<NotificationDetail>> getForChannel(String key) async {
+    final notifications = (await AwesomeNotifications()
+        .listScheduledNotifications())
+        .where((e) => e.content!.channelKey == key)
+        .map((e) {
+          final schedule = e.schedule as NotificationCalendar;
+          final date = DateTime(schedule.year!, schedule.month!, schedule.day!);
+          return NotificationDetail(id: e.content!.id!, date: date);
+        })
+        .toList();
+    notifications.sort((a,b) => a.date.compareTo(b.date));
+    return notifications;
   }
 }
