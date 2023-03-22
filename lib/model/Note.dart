57a28bfe9509b971fd71e2dcbe2d738cfdb209a0
Johannes Franke <jofra@hotmail.de>
2023-03-22 12:18:34 +0100

    Kommentar für Note.dart

--- lib/model/Note.dart
+++ lib/model/Note.dart
@@ -1,3 +1,5 @@
+/// Daten-Klasse um Informationen für eine Notiz zu speichern.
+/// Jede Notiz hat einen zugehörigen Termin, einen Titel und eine Inhalt(Body).
 class Note{
   int? id;
   int? reminderId;
@@ -16,6 +18,7 @@ class Note{
     return this;
   }
 
+  /// Methode für die Speicherung in der Datenbank.
   Map<String, Object?> toMap() {
     var map = <String, Object?>{
       "reminderId": reminderId,
@@ -28,6 +31,7 @@ class Note{
     return map;
   }
 
+  /// Methode, um Objekt aus Datenbank zu instanzinieren.
   Note.fromMap(Map<String, Object?> map):
     id = map["_id"] as int,
     reminderId = map["reminderId"] as int,
